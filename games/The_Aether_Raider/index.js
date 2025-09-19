5033a1b0ea8a278c454d548bd5f9024eee9d8291
benoit <benoit.darties@umontpellier.fr>
2025-09-19 15:09:37 +0200

    ajout de The Aether Raider

---
+++ games/The_Aether_Raider/index.js
@@ -0,0 +1,34 @@
+// chargement des librairies
+import selection from "./js/selection.js";
+import debut from "./js/debut.js";
+
+// configuration générale du jeu
+var config = {
+  width: 1280, // largeur en pixels
+  height: 720, // hauteur en pixels
+   type: Phaser.AUTO,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: 'game-container',
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  
+  },
+  physics: {
+    // définition des parametres physiques
+    default: "arcade", // mode arcade : le plus simple : des rectangles pour gérer les collisions. Pas de pentes
+    arcade: {
+      // parametres du mode arcade
+      gravity: {
+        y: 300 // gravité verticale : acceleration ddes corps en pixels par seconde
+      },
+      debug: true // afficher les hitbox et vecteurs
+    }
+  },
+  scene: [debut, selection],
+  baseURL: window.location.pathname.replace(/\/[^/]*$/, '')
+};
+
+
+// création et lancement du jeu
+var game = new Phaser.Game(config);
+game.scene.start("debut");
