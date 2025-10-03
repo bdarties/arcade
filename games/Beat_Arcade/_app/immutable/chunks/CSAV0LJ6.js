a03641b0c7db21770aacdebba08e4a94c0217910
Damien Marill <dev@marill.dev>
2025-10-03 11:51:13 +0200

    feat: beat_arcade v0.2

--- games/Beat_Arcade/_app/immutable/chunks/CSAV0LJ6.js
+++
@@ -1,2 +0,0 @@
-import{h as a}from"./CXEbYBTZ.js";a();
-//# sourceMappingURL=CSAV0LJ6.js.map
---
+++ games/Beat_Arcade/_app/immutable/entry/start.DIHxQraG.js
@@ -0,0 +1,2 @@
+import{l as o,a as r}from"../chunks/p5SjdAFO.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.DIHxQraG.js.map
