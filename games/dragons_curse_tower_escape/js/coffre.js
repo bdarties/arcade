5c5ac59c564d16aad86ad8816ef94c5c63ef6d15
cocolas007 <73226476+nico3807@users.noreply.github.com>
2025-10-03 12:41:06 +0200

    Update coffre.js

--- games/dragons_curse_tower_escape/js/coffre.js
+++ games/dragons_curse_tower_escape/js/coffre.js
@@ -10,10 +10,10 @@ export default class Coffre extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("slot_empty", "../assets/hud/slot_empty.png");
-    this.load.image("slot_selected", "../assets/hud/slot_selected.png");
-    this.load.image("slot_left", "../assets/hud/slot_left.png");
-    this.load.image("slot_right", "../assets/hud/slot_right.png");
+    this.load.image("slot_empty", "./assets/hud/slot_empty.png");
+    this.load.image("slot_selected", "./assets/hud/slot_selected.png");
+    this.load.image("slot_left", "./assets/hud/slot_left.png");
+    this.load.image("slot_right", "./assets/hud/slot_right.png");
   }
 
   create() {
