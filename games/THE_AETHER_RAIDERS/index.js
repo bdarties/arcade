cadd7f09e1623f84fb68466cdea834296edcf7e4
cocolas007 <73226476+nico3807@users.noreply.github.com>
2025-11-14 11:44:46 +0100

    Add files via upload

---
+++ games/THE_AETHER_RAIDERS/index.js
@@ -0,0 +1,36 @@
+// chargement des librairies
+import selection from "./js/selection.js";
+import debut from "./js/debut.js";
+import tutoriel from "./js/tutoriel.js";
+import boss from "./js/boss.js";
+
+// configuration générale du jeu
+var config = {
+  width: 1280, // largeur en pixels
+  height: 720, // hauteur en pixels
+   type: Phaser.AUTO,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: 'game-container',
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  
+  },
+  physics: {
+    // définition des parametres physiques
+    default: "arcade", // mode arcade : le plus simple : des rectangles pour gérer les collisions. Pas de pentes
+    arcade: {
+      // parametres du mode arcade
+      gravity: {
+        y: 300 // gravité verticale : acceleration ddes corps en pixels par seconde
+      },
+      debug: false // afficher les hitbox et vecteurs
+    }
+  },
+  scene: [debut, tutoriel, selection, boss],
+  baseURL: window.location.pathname.replace(/\/[^/]*$/, '')
+};
+
+
+// création et lancement du jeu
+export var game = new Phaser.Game(config);
+game.scene.start("debut");
